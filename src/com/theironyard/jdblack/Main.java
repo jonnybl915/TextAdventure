64aa073aec822f4da536345c2688c62e08eb0505
jonathandavidblack <jonnybl915@gmail.com>
2016-05-25 11:24:30 -0400

    add load feature

--- src/com/theironyard/jdblack/Main.java
+++ src/com/theironyard/jdblack/Main.java
@@ -1,24 +1,37 @@
 package com.theironyard.jdblack;
 
+import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
+    static final String SAVE_FILE = "game.json";
     static Scanner scanner = new Scanner(System.in); //global variable which can be used anywhere
-    static Player player = new Player();
+    static Player player;
+
     public static void main(String[] args) throws Exception {
+        player = loadGame();
+        if(player == null){
+            player = new Player();
+            System.out.println("Starting new game.");
+        }
+
         System.out.println("Welcome Traveler!");
 
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseLocation();
+        if (player.name == null) player.chooseName();
+        if (player.weapon == null) player.chooseWeapon();
+        if (player.location == null) player.chooseLocation();
 
+        if(player.items.isEmpty()) {
         player.findItem("armor");
         player.findItem("potion");
+        }
 
         Enemy orc = new Enemy("Orc", 10, 10);
         player.battle(orc);
@@ -52,7 +65,7 @@ public class Main {
         JsonSerializer serializer = new JsonSerializer();
         String json = serializer.include("*").serialize(player);
 
-        File f = new File("game.json");
+        File f = new File(SAVE_FILE);
         try {
             FileWriter fw = new FileWriter(f);
             fw.write(json);
@@ -60,5 +73,18 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Player loadGame() {
+        File f = new File(SAVE_FILE);
+        try {
+            Scanner scanner = new Scanner(f);
+            scanner.useDelimiter("\\Z");
+            String contents = scanner.next();
+            JsonParser parser = new JsonParser();
+            return parser.parse(contents, Player.class);
+        } catch (FileNotFoundException e) {
+        }
+        return null;
     }
 }
